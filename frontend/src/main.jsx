27588fb924cd6684624b267a43a219323ca3a69a
unknown <Administrator@LT06ITS0780.IPGLOBAL.CORP>
2025-12-12 10:25:43 +0100

    fix: revert explicit sw registration to fix crash

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,18 +8,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   console.log('beforeinstallprompt fired globally');
 });
 
-// Explicitly register Service Worker
-import { registerSW } from 'virtual:pwa-register';
-
-const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log('New content available, click on reload button to update.');
-  },
-  onOfflineReady() {
-    console.log('Content is cached for offline use.');
-  },
-});
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
